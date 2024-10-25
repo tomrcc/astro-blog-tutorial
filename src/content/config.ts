7ea30f76173459c6f27b8f9348ef2f140f4d221e
tomrcc <tom@cloudcannon.com>
2024-10-25 13:09:45 +1300

    Add pages collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,19 @@
 // Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
+
+const seoSchema = z
+  .object({
+    page_description: z.string().nullable(),
+    canonical_url: z.string().nullable(),
+    featured_image: z.string().nullable(),
+    featured_image_alt: z.string().nullable(),
+    author_twitter_handle: z.string().nullable(),
+    open_graph_type: z.string().nullable(),
+    no_index: z.boolean(),
+  })
+  .optional();
+
+
 // Define a `type` and `schema` for each collection
 const postsCollection = defineCollection({
     type: 'content',
@@ -15,7 +29,16 @@ const postsCollection = defineCollection({
       tags: z.array(z.string())
     })
 });
+
+const pageCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    content_blocks: z.array(z.any()),
+    seo: seoSchema,
+  })
+})
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   posts: postsCollection,
+  pages: pageCollection
 };
