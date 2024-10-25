4767c7eb5ae1900c340f0028cda21dcb0d186c9e
tomrcc <tom@cloudcannon.com>
2024-10-25 13:18:29 +1300

    Add Bookshop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
+import bookshop from "@bookshop/astro-bookshop";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact()]
+  integrations: [preact(), bookshop()],
 });
